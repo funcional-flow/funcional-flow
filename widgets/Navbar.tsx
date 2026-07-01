169b9371cb250d7f0b5939699adf0c8731a279db
DoRenato <64191958+DoRenato@users.noreply.github.com>
2026-07-01 19:44:10 -0300

    Update Navbar.tsx

--- widgets/Navbar.tsx
+++ widgets/Navbar.tsx
@@ -1,45 +1,45 @@
-"use client";
+// "use client";
 
-import { useState } from "react";
-import { Spin as Hamburger } from "hamburger-react";
-import { Button } from "@/components/ui/button";
-import {
-  Sheet,
-  SheetContent,
-  SheetDescription,
-  SheetHeader,
-  SheetTitle,
-  SheetTrigger,
-} from "@/components/ui/sheet";
+// import { useState } from "react";
+// import { Spin as Hamburger } from "hamburger-react";
+// import { Button } from "@/components/ui/button";
+// import {
+//   Sheet,
+//   SheetContent,
+//   SheetDescription,
+//   SheetHeader,
+//   SheetTitle,
+//   SheetTrigger,
+// } from "@/components/ui/sheet";
 
-export default function Navbar() {
-  const [Open, setOpen] = useState(false);
-  return (
-    <nav className="fixed right-0 z-1000 w-full bg-white py-2">
-      <div className="flex items-center justify-between">
-        <div className="pl-4 text-2xl font-bold">Badiamba</div>
-        <div className="relative z-1001 pr-2">
-          <Hamburger toggled={Open} toggle={setOpen} />
-        </div>
-        <Sheet open={Open} onOpenChange={setOpen}>
-          <SheetContent showCloseButton={false} side="right" className="z-990">
-            <div className="flex justify-end pt-4 pr-4">
-              <Hamburger toggled={Open} toggle={setOpen} />
-            </div>
-            <SheetHeader>
-              <SheetTitle>Badiamba</SheetTitle>
-              <SheetDescription>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              </SheetDescription>
-              <div className="pt-10 flex flex-col gap-5">
-                <a href="#home">Home</a>
-                <a href="#about">About</a>
-                <a href="#contact">Contact</a>
-              </div>
-            </SheetHeader>
-          </SheetContent>
-        </Sheet>
-      </div>
-    </nav>
-  );
-}
+// export default function Navbar() {
+//   const [Open, setOpen] = useState(false);
+//   return (
+//     <nav className="fixed right-0 z-1000 w-full bg-white py-2">
+//       <div className="flex items-center justify-between">
+//         <div className="pl-4 text-2xl font-bold">Badiamba</div>
+//         <div className="relative z-1001 pr-2">
+//           <Hamburger toggled={Open} toggle={setOpen} />
+//         </div>
+//         <Sheet open={Open} onOpenChange={setOpen}>
+//           <SheetContent showCloseButton={false} side="right" className="z-990">
+//             <div className="flex justify-end pt-4 pr-4">
+//               <Hamburger toggled={Open} toggle={setOpen} />
+//             </div>
+//             <SheetHeader>
+//               <SheetTitle>Badiamba</SheetTitle>
+//               <SheetDescription>
+//                 Lorem ipsum dolor sit amet consectetur adipisicing elit.
+//               </SheetDescription>
+//               <div className="pt-10 flex flex-col gap-5">
+//                 <a href="#home">Home</a>
+//                 <a href="#about">About</a>
+//                 <a href="#contact">Contact</a>
+//               </div>
+//             </SheetHeader>
+//           </SheetContent>
+//         </Sheet>
+//       </div>
+//     </nav>
+//   );
+// }
