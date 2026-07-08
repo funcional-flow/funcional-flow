b62393f6711c425459827b56eb12dea9c9ff3032
DoRenato <64191958+DoRenato@users.noreply.github.com>
2026-07-07 22:11:50 -0300

    animação adicionada

--- components/CardDif.tsx
+++ components/CardDif.tsx
@@ -6,8 +6,8 @@ interface CardDifProps {
 export default function CardDif({ titulo, descricao }: CardDifProps) {
   return (
     <div className="relative">
-      <div className="bg-yellow3 absolute top-0 left-0 px-10 py-1" />
-      <div className="bg-purple2 absolute top-1 left-1 px-10 py-1" />
+      <div className="bg-yellow3 absolute top-0 animate-stripe-back left-0 px-10 py-1" />
+      <div className="bg-purple2 absolute top-1 animate-stripe-front left-1 px-10 py-1" />
       <div className="relative flex flex-col top-8">
         <h3 className="text-xl font-bold text-purple3">{titulo}</h3>
         <p>{descricao}</p>
